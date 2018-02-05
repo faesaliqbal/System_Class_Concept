616dabcb6f4be22001bb02f3e9829c3a38d3fa50
faisalebricks <faisal.iqbal@ebricks-inc.com>
2018-02-05 17:10:09 +0500

    initial commit

---
+++ System_Class_Concept/src/package1/MyClass.java
@@ -0,0 +1,38 @@
+package package1;
+
+public class MyClass {
+
+	public static void main(String[] args) {
+		
+		System.out.println(System.getenv());
+		System.out.println(System.getProperties());
+		
+		System.out.println(System.getProperty("java.version"));
+		System.out.println(System.getProperty("java.vendor"));
+		System.out.println(System.getProperty("java.vendor.url"));
+		System.out.println(System.getProperty("java.home"));
+		System.out.println(System.getProperty("java.vm.specification.version"));
+		System.out.println(System.getProperty("java.vm.specification.vendor"));
+		System.out.println(System.getProperty("java.vm.specification.name"));
+		System.out.println(System.getProperty("java.vm.version"));
+		System.out.println(System.getProperty("java.vm.specification.version"));
+		System.out.println(System.getProperty("java.vm.specification.vendor"));
+		System.out.println(System.getProperty("java.vm.specification.name"));
+		System.out.println(System.getProperty("java.class.version"));
+		System.out.println(System.getProperty("java.class.path"));
+		System.out.println(System.getProperty("java.library.path"));
+		
+		System.out.println(System.getProperty("java.compiler"));
+		System.out.println(System.getProperty("os.name"));
+		System.out.println(System.getProperty("os.arch"));
+		System.out.println(System.getProperty("os.version"));
+		
+		System.out.println(System.getProperty("user.name"));
+		System.out.println(System.getProperty("user.home"));
+		System.out.println(System.getProperty("user.dir"));
+		
+		
+
+	}
+
+}
